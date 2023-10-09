ab5469559b45fd3caa8b4abcf00aa4c18dcc2341
Md. Aktharuzzaman <aktharuzzaman9334@gmail.com>
2023-10-09 14:09:39 +0600

    camera changed

--- chepest.js
+++ chepest.js
@@ -2,7 +2,7 @@ const phones = [
     { name: 'samsung', camera: 64, storage: '32gb', price: 36000, color: 'silver' },
     { name: 'walton', camera: 32, storage: '32gb', price: 22000, color: 'silver' },
     { name: 'iphone', camera: 12, storage: '512gb', price: 136000, color: 'silver' },
-    { name: 'Xiaomi', camera: 120, storage: '32gb', price: 30000, color: 'silver' },
+    { name: 'Xiaomi', camera: 50, storage: '32gb', price: 30000, color: 'silver' },
     { name: 'oppo', camera: 48, storage: '32gb', price: 16000, color: 'silver' },
     { name: 'Oneplus', camera: 84, storage: '32gb', price: 56000, color: 'silver' },
     { name: 'Nokia', camera: 8, storage: '32gb', price: 6000, color: 'silver' },
